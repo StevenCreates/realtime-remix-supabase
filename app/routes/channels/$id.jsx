d12739a1bec6b75a0f52bd488849cbbc216d3ca5
StevenCreates <steven@stevencreates.tech>
2022-04-13 15:17:57 -0600

    enabled live db for messages

--- app/routes/channels/$id.jsx
+++ app/routes/channels/$id.jsx
@@ -1,10 +1,12 @@
-import { useLoaderData } from "@remix-run/react";
+import { useLoaderData, Form, useFetcher } from "@remix-run/react";
 import supabase from "~/utils/supabase";
+import { useEffect, useState } from "react";
 
+// Load data
 export const loader = async ({ params: { id } }) => {
   const { data: channel, error } = await supabase
     .from("channels")
-    .select("title, description, messages(id, content)")
+    .select("id, title, description, messages(id, content)")
     .match({ id })
     .single();
   if (error) {
@@ -16,26 +18,47 @@ export const loader = async ({ params: { id } }) => {
   };
 };
 
+// Mutate Data
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const content = formData.get("content");
+  const channelId = formData.get("channelId");
+  const { error } = await supabase
+    .from("messages")
+    .insert({ content, channel_id: channelId });
+  if (error) {
+    console.log(error.message);
+  }
+  return null;
+};
+
 export default () => {
   const { channel } = useLoaderData();
+  const [messages, setMessages] = useState([...channel.messages]);
+  const fetcher = useFetcher();
 
-  const handleSubmit = async (e) => {
-      e.preventDefault();
-      const formData = new FormData(e.target);
-      const content = formData.get("content");
-      const {data, error} = await supabase.from('message').insert({content, channel_id: channel.id})
-      if (error) {
-          console.log(error.message)
-      }
-      console.log({data})
-  }
+  useEffect(() => {
+    supabase
+      .from(`messages:channel_id=eq.${channel.id}`)
+      .on("*", (payload) => {
+        setMessages(current => [...current, {id: payload.new.id, content: payload.new.content}]);
+        fetcher.load(`/channels/${channel.id}`)
+      })
+      .subscribe();
+  }, []);
+
+  useEffect(() => {
+    setMessages([...channel.messages]);
+  }, [channel])
+
   return (
     <div>
-      <pre key={channel}>{JSON.stringify(channel, null, 2)}</pre>
-      <form onSubmit={handleSubmit}>
-          <input name="content" />
-          <button>Send!</button>
-      </form>
+      <pre key={channel}>{JSON.stringify(messages, null, 2)}</pre>
+      <Form method="post">
+        <input name="content" />
+        <input type="hidden" name="channelId" value={channel.id} />
+        <button>Send!</button>
+      </Form>
     </div>
   );
 };
