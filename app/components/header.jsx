e6dde4267d603b487d4e7ea6fb429b52ab5bc3ef
StevenCreates <steven@stevencreates.tech>
2022-05-06 14:05:00 -0600

    working on landing page

---
+++ app/components/header.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+
+const navigation = [
+    { name: 'Solutions', href: '#' },
+    { name: 'Pricing', href: '#' },
+    { name: 'Docs', href: '#' },
+    { name: 'Company', href: '#' },
+  ]
+
+
+export const Header = () => {
+  return (
+    <header className=" bg-zinc-800 mb-8">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+        <div className="w-full py-3 flex items-center justify-between border-b border-zinc-400 md:border-none lg:border-none">
+          <div className="flex items-center">
+            <a href="#">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-10 w-auto"
+                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                alt=""
+              />
+            </a>
+            <div className="hidden ml-10 space-x-8 md:block lg:block">
+              {navigation.map((link) => (
+                <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+                  {link.name}
+                </a>
+              ))}
+            </div>
+          </div>
+          <div className="ml-10 space-x-4">
+            <button
+              href="#"
+              className="inline-block bg-violet-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
+            >
+              Sign in
+            </button>
+            <button
+              href="#"
+              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-violet-500 hover:bg-fuchsia-50"
+            >
+              Sign up
+            </button>
+          </div>
+        </div>
+        <div className="py-4 flex flex-wrap justify-center space-x-6 md:hidden lg:hidden">
+          {navigation.map((link) => (
+            <a key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </header>
+  )
+}
